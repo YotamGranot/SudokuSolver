b5474d355c2acd3403ceb8e5322c7c8ad193bddf
Yotam Granot <granot.yotam@gmail.com>
2020-05-05 14:16:16 +0300

    added ability to insert your own sudoku

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         // write your code here
-        int gridToSolve[][] = {{8, 0, 0, 4, 0, 6, 0, 0, 3},
-                {0, 0, 9, 0, 0, 0, 0, 2, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 8, 0, 0, 4, 0, 0},
-                {0, 6, 0, 0, 0, 0, 0, 1, 0},
-                {0, 0, 3, 0, 0, 2, 0, 0, 9},
-                {7, 0, 2, 0, 3, 0, 0, 0, 0},
-                {0, 4, 0, 0, 0, 0, 5, 0, 0},
-                {5, 0, 1, 7, 0, 9, 0, 0, 8}};
+        int gridToSolve[][] = new int[9][9];
+        System.out.println("Enter Matrix Data");
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                gridToSolve[i][j] = in.nextInt();
+            }
+        }
         Grid sudoku = new Grid(gridToSolve);
         System.out.println("this is the grid: ");
         sudoku.printGrid();
-        if(sudoku.solveGrid()){
+        if (sudoku.solveGrid()) {
             System.out.println("successful solve \nthis is the solved grid:");
             sudoku.printGrid();
-        }
-        else{
+        } else {
             System.out.println("unsolvable sudoku");
         }
     }
